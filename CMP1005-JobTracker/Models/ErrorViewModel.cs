ff2ae88ddd7e0816ca8061bc6902c335f7827911
irsupera <97858090+irsupera@users.noreply.github.com>
2022-08-04 15:31:48 -0400

    Initial commit
    Initial commit

---
+++ CMP1005-JobTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CMP1005_JobTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
