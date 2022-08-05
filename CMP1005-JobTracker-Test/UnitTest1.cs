8bc2507cc02ad01714c72f012927b75739a7f25c
irsupera <97858090+irsupera@users.noreply.github.com>
2022-08-04 23:50:01 -0400

    Added MS test for Job
    Added MS test for Job (not yet working - with errors in codes)

---
+++ CMP1005-JobTracker-Test/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CMP1005_JobTracker.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CMP1005_JobTracker_Test
+{
+    [TestClass]
+    public class UnitTest1: ControllerBase
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            JobTrackerController controller = new JobTrackerController();
+            ViewResult result = controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void TestDetails()
+        {
+            JobTrackerController controller = new JobTrackerController();
+            ViewResult result = controller.Details(2) as ViewResult;
+            Assert.AreEqual("Details", result.ViewName);
+        }
+    }
+}
+
