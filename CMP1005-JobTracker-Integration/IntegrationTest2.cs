d846012bb201d32e7cd0a26637744b6436f4b2f5
irsupera <97858090+irsupera@users.noreply.github.com>
2022-08-15 15:37:42 -0400

    Created additional integration test
    Created additional integration test for DTR and Reminder view - create page (fill values to fields). Added id attribute to fields in the create page for both.

---
+++ CMP1005-JobTracker-Integration/IntegrationTest2.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Safari;
+//using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace CMP1005_JobTracker_Integration
+{
+    [TestClass]
+    public class IntegrationTest2
+    {
+        private IWebDriver _webDriver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            //new DriverManager().SetUpDriver(new ChromeConfig());
+            //_webDriver = new ChromeDriver();
+            //_webDriver.Url = "https://localhost:5001/DTR/Create";
+            //_webDriver.Navigate();
+
+            _webDriver = new SafariDriver();
+            _webDriver.Navigate().GoToUrl("https://localhost:5001/DTR/Create");
+            _webDriver.Navigate().Refresh();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var input_pos = _webDriver.FindElement(By.Id("TimeIn"));
+            input_pos.Click();
+            input_pos.SendKeys("08/14/2022 08:00");
+
+            var input_com = _webDriver.FindElement(By.Id("TimeOut"));
+            input_pos.Click();
+            input_pos.SendKeys("08/14/2022 17:00");
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            _webDriver.Quit();
+        }
+    }
+}
---
+++ CMP1005-JobTracker-Integration/IntegrationTest3.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Safari;
+//using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace CMP1005_JobTracker_Integration
+{
+    [TestClass]
+    public class IntegrationTest3
+    {
+        private IWebDriver _webDriver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            //new DriverManager().SetUpDriver(new ChromeConfig());
+            //_webDriver = new ChromeDriver();
+            //_webDriver.Url = "https://localhost:5001/Reminder/Create";
+            //_webDriver.Navigate();
+
+            _webDriver = new SafariDriver();
+            _webDriver.Navigate().GoToUrl("https://localhost:5001/Reminder/Create");
+            _webDriver.Navigate().Refresh();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var input_pos = _webDriver.FindElement(By.Id("Detail"));
+            input_pos.Click();
+            input_pos.SendKeys("Follow-up schedule for next week");
+
+            var input_com = _webDriver.FindElement(By.Id("DateTime"));
+            input_pos.Click();
+            input_pos.SendKeys("08/15/2022 08:00");
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            _webDriver.Quit();
+        }
+    }
+}
