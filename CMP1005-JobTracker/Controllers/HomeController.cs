4e51b1c4b800868211841a71aa20d2c59bf2b505
irsupera <97858090+irsupera@users.noreply.github.com>
2022-08-19 00:18:59 -0400

    Refined views/pages
    Refined views/pages

--- CMP1005-JobTracker/Controllers/HomeController.cs
+++ CMP1005-JobTracker/Controllers/HomeController.cs
@@ -26,18 +26,6 @@ namespace CMP1005_JobTracker.Controllers
             return View(await jobTrackerContext.ToListAsync());
         }
 
-        //private readonly ILogger<HomeController> _logger;
-
-        //public HomeController(ILogger<HomeController> logger)
-        //{
-        //    _logger = logger;
-        //}
-
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
-
         public IActionResult Privacy()
         {
             return View();
